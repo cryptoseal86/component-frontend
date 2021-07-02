3689e1c8a94377a9a6161fac59a3ae788d09b04b
Spiridon Robakidze <spiridonelectro@gmail.com>
2021-07-02 15:30:38 +0200

    fixed merge issue

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -28,7 +28,7 @@ export default function (){
   return (
     <StyledLogo>
       <StyledImg src={logo} />
-      <StyledName>component on BSC</StyledName>
+      <StyledName>component on xDAI</StyledName>
     </StyledLogo>
   )
 }
